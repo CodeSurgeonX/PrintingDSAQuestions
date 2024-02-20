fd3c02e840c65be67b2178b144511e90061c75b9
Shashwat Kashyap <shashwat.kashyap@icloud.com>
2024-02-20 20:26:51 +0530

    Finished

--- PrintingDSAQuestions/Questions/SeachingInSorted2DMatrix.swift
+++ PrintingDSAQuestions/Questions/SeachingInSorted2DMatrix.swift
@@ -16,12 +16,34 @@ struct SeachingInSorted2DMatrixBinarySearchWay: Solution {
                     let columnIndex = binarySearch(matrix[row], value)
                     if columnIndex >= 0 {
                         print("Found element at index \(row), \(columnIndex)")
+                        return
                     }
                 }
             }
             print("Not found")
         }
         
+        func searchInFlattenedMatrix(matrix: [[Int]], value: Int) {
+            let numberOfRows = matrix.count
+            let numberOfColumns = matrix[0].count
+            var left = 0
+            var right = (numberOfRows * numberOfColumns) - 1
+            
+            while left <= right {
+                let mid = left + (right - left)/2
+                // Only this part differs when compared to plain binary search because we are using math to flatten the array
+                if matrix[mid/numberOfColumns][mid%numberOfColumns] == value {
+                    print("Found number at index \(mid/numberOfColumns) \(mid%numberOfColumns)")
+                    return
+                } else if matrix[mid/numberOfColumns][mid%numberOfColumns] < value {
+                    left = mid + 1
+                } else {
+                    right = mid - 1
+                }
+            }
+        }
+        
+        //MARK: Helper
         func binarySearch(_ nums: [Int], _ target: Int) -> Int {
             var left = 0
             var right = nums.count - 1
@@ -39,6 +61,6 @@ struct SeachingInSorted2DMatrixBinarySearchWay: Solution {
             return -1
         }
         
-        search(matrix: [[1, 3, 7], [12, 21, 23], [54, 55, 70] ], value: 54)
+        searchInFlattenedMatrix(matrix: [[1, 3, 7], [12, 21, 23], [54, 55, 70] ], value: 7) //0,2
     }
 }
